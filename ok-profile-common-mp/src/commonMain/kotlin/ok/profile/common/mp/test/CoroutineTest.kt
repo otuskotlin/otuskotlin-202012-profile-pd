9d8391f37e06ff2f9caa736f9571cdea0290730d
denis <denispavlov1989@gmail.com>
2021-04-01 09:49:05 +0700

    add kmp test runner

---
+++ ok-profile-common-mp/src/commonMain/kotlin/ok/profile/common/mp/test/CoroutineTest.kt
@@ -0,0 +1,7 @@
+package ok.profile.common.mp.test
+
+import kotlinx.coroutines.CoroutineScope
+import kotlin.coroutines.CoroutineContext
+
+expect fun runBlockingTest(block: suspend CoroutineScope.()-> Unit)
+expect val testCoroutineContext: CoroutineContext
